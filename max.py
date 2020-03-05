001450cd7d9174b72ad44a49678c096c2eec71e9
MWANGANGI1999 <stvnmunene@gmail.com>
2020-03-05 11:05:01 +0300

    finding the max of three numbers

---
+++ max.py
@@ -0,0 +1,9 @@
+# Python program to find the max number among the three numbers 
+def maximum(a, b, c): 
+   list = [a, b, c] 
+   return max(list) 
+# Driven code  
+x = int(input("Enter First number="))
+y = int(input("Enter Second number="))
+z = int(input("Enter Third number="))
+print("Maximum Number is ::",maximum(x, y, z)) 
